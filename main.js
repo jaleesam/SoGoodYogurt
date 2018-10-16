c75e6f862d339a9cb053b3499e30daed4152dfd4
Jaleesa McCrea <jmccrea01@gmail.com>
2018-10-15 23:52:23 -0400

    js model code added

--- main.js
+++ main.js
@@ -1,9 +1,29 @@
-const nav = document.querySelector('#navvie');
-const navTop = nav.offsetTop;
+//FOR STICKY NAVBAR
+// const nav = document.querySelector('#navvie');
+// const navTop = nav.offsetTop;
 
-function stickyNavigation() { 
-    console.log('navTop = ' + navTop);
-    console.log('scrollY = ' + window.scrollY);
-  }
+// function stickyNavigation() { 
+//     console.log('navTop = ' + navTop);
+//     console.log('scrollY = ' + window.scrollY);
+//   }
   
-  window.addEventListener('scroll', stickyNavigation);
+//   window.addEventListener('scroll', stickyNavigation);
+
+//FOR MODAL
+var  modal = document.querySelector(".modal");
+var trigger = document.querySelector(".trigger");
+var closeButton = document.querySelector(".close-button");
+
+function toggleModal(){
+  modal.classList.toggle("show-modal");
+}
+
+function windowOnClick(event){
+  if (event.target === modal) {
+    toggleModal();
+  }
+}
+trigger.addEventListener("click", toggleModal);
+    closeButton.addEventListener("click", toggleModal);
+    window.addEventListener("click", windowOnClick);
+	
